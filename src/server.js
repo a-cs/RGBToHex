c41556da331e928cbbe28627c6338466a962e01d
a-cs <acarneirosousa@gmail.com>
2022-05-21 12:30:14 -0300

    parte 2

---
+++ src/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+
+const convert = require("./convert")
+
+const port = 8080;
+
+const app = express()
+app.use(express.json())
+
+app.post('/rgbToHex', (req, res) => {
+	const {red, blue, green} = req.body
+	try{
+		const hex = convert.RGBToHex(red, blue, green)
+		const myJson = {hex}
+		res.json(myJson)
+	} catch (error) {
+		console.log(error.message)
+		const { message} = error
+		const myJson = {message}
+		res.json(myJson)
+	}
+})
+
+app.post('/hextoRgb', (req, res) => {
+	const {hex} = req.body
+	try{
+	const rgb = convert.HexToRGB(hex)
+	res.json(rgb)
+	}
+	catch (error) {
+		console.log(error.message)
+		const { message} = error
+		const myJson = {message}
+		res.json(myJson)
+	}
+})
+
+
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
